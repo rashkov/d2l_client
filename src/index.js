029b6973f73d8e8fed2ef2907a8de20561b00b5d
Michael Rashkovsky <mrashkovsky@gmail.com>
2018-07-03 00:09:40 -0400

    Disable service worker

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
+//import registerServiceWorker from "./registerServiceWorker";
+import { unregister } from "./registerServiceWorker";
 
 ReactDOM.render(<App rows="40" cols="25" />, document.getElementById("root"));
-registerServiceWorker();
+//registerServiceWorker();
+unregister();
