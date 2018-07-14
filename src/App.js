e3ad178079d604c66e67211eb87269d43e512b00
Michael Rashkovsky <mrashkovsky@gmail.com>
2018-07-14 12:55:37 -0400

    Refactor jsx into components

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import React, { Component } from "react";
 import * as _ from "lodash";
 import * as d2l_logo from "./d2l_logo.png";
 import data from "./class_sample_json.js";
+import table from "./table.jsx.js";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      logged_in: false,
+      logged_in: true,
       retry: false,
       classroom: data
     };
@@ -24,6 +25,82 @@ class App extends Component {
       }, 1000);
     }
   }
+  exchangeColumnJSX(exchange) {
+    let color;
+    if (exchange.type === "student_to_mentor") {
+      color = "#48ab97";
+    } else if (exchange.type === "mentor_to_student") {
+      color = "#21265e";
+    } else {
+      color = "#ffffff";
+    }
+    return (
+      <div
+        key={exchange.id}
+        style={{
+          display: "inline-block",
+          width: "30px",
+          height: "30px",
+          backgroundColor: color,
+          border: "1px solid black",
+          margin: "5px",
+          marginBottom: "-5px",
+          borderRadius: "5px"
+        }}
+      />
+    );
+  }
+  getStudentRowJSX(student, exchange_columns) {
+    return (
+      <div>
+        <div
+          style={{
+            display: "inline-block",
+            width: "50px",
+            height: "30px"
+          }}
+        >
+          {student.name}
+        </div>
+        <div style={{ display: "inline-block", width: "50px", height: "30px" }}>
+          {student.mentor.name}
+        </div>
+        {exchange_columns}
+      </div>
+    );
+  }
+  getLoginJSX() {
+    return (
+      <div>
+        <div
+          style={{
+            opacity: this.state.retry ? 1 : 0,
+            marginBottom: "10px",
+            color: "red"
+          }}
+        >
+          Please try again
+        </div>
+        <form style={{ width: "50%" }} onSubmit={this.login.bind(this)}>
+          <div className="form-group">
+            <label htmlFor="exampleInputPassword1">Password</label>
+            <input
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+              placeholder="Password"
+              ref={input => {
+                this.textInput = input;
+              }}
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </div>
+    );
+  }
   render() {
     let main,
       rows = [];
@@ -32,121 +109,15 @@ class App extends Component {
       console.log(student);
       let exchange_columns = [];
       _.each(student.exchanges, exchange => {
-        let color;
-        if (exchange.type === "student_to_mentor") {
-          color = "#48ab97";
-        } else if (exchange.type === "mentor_to_student") {
-          color = "#21265e";
-        } else {
-          color = "#ffffff";
-        }
-        exchange_columns.push(
-          <div
-            key={exchange.id}
-            style={{
-              display: "inline-block",
-              width: "30px",
-              height: "30px",
-              backgroundColor: color,
-              border: "1px solid black",
-              margin: "5px",
-              marginBottom: "-5px",
-              borderRadius: "5px"
-            }}
-          />
-        );
+        exchange_columns.push(this.exchangeColumnJSX(exchange));
       });
-      let student_row = (
-        <div>
-          <div
-            style={{
-              display: "inline-block",
-              width: "50px",
-              height: "30px",
-              lineHeight: "30px"
-            }}
-          >
-            {student.name}
-          </div>
-          <div
-            style={{ display: "inline-block", width: "50px", height: "30px" }}
-          >
-            {student.mentor.name}
-          </div>
-          {exchange_columns}
-        </div>
-      );
+      let student_row = this.getStudentRowJSX(student, exchange_columns);
       rows.push(student_row);
     });
     if (!this.state.logged_in) {
-      main = (
-        <div>
-          <div
-            style={{
-              opacity: this.state.retry ? 1 : 0,
-              marginBottom: "10px",
-              color: "red"
-            }}
-          >
-            Please try again
-          </div>
-          <form style={{ width: "50%" }} onSubmit={this.login.bind(this)}>
-            <div className="form-group">
-              <label htmlFor="exampleInputPassword1">Password</label>
-              <input
-                type="password"
-                className="form-control"
-                id="exampleInputPassword1"
-                placeholder="Password"
-                ref={input => {
-                  this.textInput = input;
-                }}
-              />
-            </div>
-            <button type="submit" className="btn btn-primary">
-              Submit
-            </button>
-          </form>
-        </div>
-      );
+      main = this.getLoginJSX();
     } else {
       main = rows;
-      /* main = (
-       *   <div>
-       *     {_.range(0, this.props.rows).map(i => {
-       *       return (
-       *         <div key={i}>
-       *           {_.range(0, this.props.cols).map(j => {
-       *             let color,
-       *               rnd = Math.random();
-       *             if (rnd > 0.8) {
-       *               color = "#48ab97";
-       *             } else if (rnd < 0.8 && rnd > 0.6) {
-       *               color = "#ffffff";
-       *             } else {
-       *               color = "#21265e";
-       *             }
-
-       *             return (
-       *               <div
-       *                 key={j}
-       *                 style={{
-       *                   display: "inline-block",
-       *                   width: "30px",
-       *                   height: "30px",
-       *                   backgroundColor: color,
-       *                   border: "1px solid black",
-       *                   margin: "5px",
-       *                   borderRadius: "5px"
-       *                 }}
-       *               />
-       *             );
-       *           })}
-       *         </div>
-       *       );
-       *     })}
-       *   </div>
-       * ); */
     }
     return (
       <div
