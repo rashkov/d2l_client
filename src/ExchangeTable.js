cf2d8af6757e141ec0f796e285c52f61e737d6f0
Michael Rashkovsky <mrashkovsky@gmail.com>
2018-07-20 16:50:48 -0400

    Fix up table order

--- src/ExchangeTable.js
+++ src/ExchangeTable.js
@@ -10,9 +10,28 @@ class ExchangeTable extends Component {
   }
   render() {
     let a = _.chain(this.state.classroom.students)
-      .flatMap((all_exchanges, index, student) => {
-        console.log(student);
+      .flatMap((student) => {
         return student.exchanges;
+      })
+      .filter((exchange)=>{
+        let allowed_types = ['mentor_to_student', 'student_to_mentor'];
+        let released = !_.isEmpty(exchange.release_date);
+        return allowed_types.indexOf(exchange.type) != -1 && released;
+      })
+      .orderBy(['release_date'])
+      .reduce((exchanges, exch)=>{
+        if(exchanges.length == 0){
+          exch.column = 0;
+          return [exch];
+        }else{
+          let previous = exchanges[exchanges.length-1];
+          if(previous.type == exch.type){
+            exch.column = previous.column;
+          }else{
+            exch.column = previous.column + 1;
+          }
+          return exchanges.concat(exch);
+        }
       }, [])
       .value();
     console.log(a);
