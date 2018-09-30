22f29bddb41b3d8a8f5ec6113c7c22bc6bb1186e
Michael Rashkovsky <mrashkovsky@gmail.com>
2018-09-29 23:39:00 -0400

    Refactor and adjust to the new server's different data structure

--- src/ExchangeTable.js
+++ src/ExchangeTable.js
@@ -24,80 +24,104 @@ class ExchangeTable extends Component {
     let main;
     if (this.state.session) {
       console.log("sesh", this.state.session);
+
       let max_column = 0;
-      let a = _.chain(this.state.session.exchanges)
-        .filter(exchange => {
-          return !_.isEmpty(exchange.date_released);
+
+      // indexed emails looks like this: { 11: {9: [email1, email2, email3 ]}}
+      // where 11 is the student_id and 9 is volunteer_id
+      let indexed_emails = _.chain(this.state.session.emails)
+        .filter(email => {
+          return email.state == "Sent"; // Only look at sent emails
         })
-        .orderBy(["date_released"])
-        .reduce((exchanges, exch) => {
-          if (exchanges.length == 0) {
-            exch.column = 0;
-            return [exch];
+        .orderBy(["envelope_datetime"]) // Order by datetime, making this a chronological list of emails coming into the system
+        //.tap(console.log)
+        .reverse()
+        .reduce((emails, email) => {
+          // Assign a column to each email.
+          // Create a new column when two emails which are next to each other chronologically switch from one exchange_type to another
+          // Where exchange type is either: student_to_volunteer or volunteer_to_student
+          // This should have the effect of drawing a table where each column only contains one type of email:
+          //   student_to_volunteer (displayed with a green square), or volunteer_to_student (displayed with a blue square)
+          if (emails.length == 0) {
+            email.column = 0;
+            return [email];
           } else {
-            let previous = exchanges[exchanges.length - 1];
-            if (previous.exchange_type == exch.exchange_type) {
-              exch.column = previous.column;
+            let previous = emails[emails.length - 1];
+            if (previous.exchange_type == email.exchange_type) {
+              email.column = previous.column;
             } else {
-              exch.column = previous.column + 1;
-            }
-            if (exch.column > max_column) {
-              max_column = exch.column;
+              email.column = previous.column + 1;
             }
 
-            console.log(exch.column);
-            return exchanges.concat(exch);
+            if (email.column > max_column) {
+              max_column = email.column;
+            }
+
+            return emails.concat(email);
           }
         }, [])
-        .reduce((indexed_exchanges, exch) => {
-          indexed_exchanges[exch.student_id] =
-            indexed_exchanges[exch.student_id] || {};
-          indexed_exchanges[exch.student_id][exch.volunteer_id] =
-            indexed_exchanges[exch.student_id][exch.volunteer_id] || [];
-          indexed_exchanges[exch.student_id][exch.volunteer_id].push(exch);
-          return indexed_exchanges;
+        //.tap(console.log)
+        .reduce((indexed_emails, email) => {
+          indexed_emails[email.student_id] =
+            indexed_emails[email.student_id] || {};
+          indexed_emails[email.student_id][email.volunteer_id] =
+            indexed_emails[email.student_id][email.volunteer_id] || [];
+          indexed_emails[email.student_id][email.volunteer_id].push(email);
+          return indexed_emails;
         }, {})
+        //.tap(console.log)
         .value();
-      console.log("a", a);
-      console.log(max_column);
 
+      console.log("indexed emails: ", indexed_emails);
+      console.log("number of columns in table: ", max_column);
+
+      // Generate the table rows. One row per student/volunteer combination
       let rows = [];
-      _.each(a, (volunteers, student_id)=>{
-        _.each(volunteers, (emails, volunteer_id)=>{
-          let row = <tr>
-            {_.chain(_.range(0, max_column))
-              .reverse()
-              .map((column)=>{
-                let emails_in_column = _.filter(emails, { column: column });
-                if (emails_in_column.length){
-                  return <td>{_.map(emails_in_column, (email)=>{
-                    return <Square key={email.id} id={email.id} email={email} />;
-                  })}</td>;
-                }else{
-                  return <td/>;
-                }
-              })
-              .value()
-            }
-          </tr>;
+      _.each(indexed_emails, (volunteers, student_id) => {
+        _.each(volunteers, (emails, volunteer_id) => {
+          let row = (
+            <tr>
+              {_.chain(_.range(0, max_column + 1))
+                .map(column => {
+                  console.log("col", column);
+                  let emails_in_column = _.filter(emails, { column: column });
+                  if (emails_in_column.length) {
+                    return (
+                      <td>
+                        {_.map(emails_in_column, email => {
+                          return (
+                            <Square
+                              key={email.id}
+                              id={email.id}
+                              email={email}
+                            />
+                          );
+                        })}
+                      </td>
+                    );
+                  } else {
+                    return <td />;
+                  }
+                })
+                .value()}
+            </tr>
+          );
           rows.push(row);
         });
       });
       main = (
-        <Table>
+        <Table bordered>
           <thead>
             <tr className="exchange-head">
-            {_.chain(_.range(0, max_column))
-              .reverse()
-              .map(col => {
-                return <th>{col}</th>;
-              })
-              .value()}
+              {_.chain(_.range(0, max_column + 1))
+                .reverse()
+                .map(col => {
+                  return <th>{col}</th>;
+                })
+                .value()}
             </tr>
           </thead>
-          <tbody>
-            {rows}
-          </tbody>
+          <tbody>{rows}</tbody>
         </Table>
       );
     } else {
